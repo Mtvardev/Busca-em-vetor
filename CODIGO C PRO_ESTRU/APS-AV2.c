1a28d8353078e232f1c95cbeb5126a1a0510e14d
mateus varelo <mateusferreira703@gmail.com>
2020-11-01 00:32:27 -0300

    11/2020
    Pronto

--- CODIGO C PRO_ESTRU/APS-AV2.c
+++ CODIGO C PRO_ESTRU/APS-AV2.c
@@ -1,56 +1,115 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define TAM 25
-#define TAM2 10
+#define TAM2 4
+
 void Menu();
 void Cadastro();
 void Listar();
-void CriaArquivo();
+void Arquivo();
+void AlocarMemoria();
 //Estrutura do tipo vetor de registro
+
 typedef struct 
 {
- char nome[TAM],matricula[TAM2];
- float media;   
+ char nome[TAM],cod[TAM2];  
+ float media;
 }DadosAluno;
-
 //variaveis globais
-DadosAluno aluno;
+ DadosAluno *aluno;
 int Opcao,total;
+        
         main()
         {
         Menu();
         }
 void Menu()
 {
- printf("-----------------------");   
- printf("\n       Menu");
- printf("\n-----------------------");  
- printf("\nESCOLHA UMA OPCAO:");
- printf("\n1-Entrar com novos dados :");
- printf("\n2-Listar Dados:");
- printf("\n3-Sair:\n");
- scanf("%d",&Opcao);
- system("cls");
-    switch (Opcao)
-    {
-    case 1:
-        Cadastro();
-        break;
-    case 2:
-        //Listar();
-        break;
-    case 3:
-        printf("Saindo.......");
-        printf("\n.....");
-        break;
-    default:
-        printf("Opcao invalida! Tente novamente.....\n");
-        Menu();
-        break;
-    }
+                printf("-----------------------");   
+                printf("\n       Menu");
+                printf("\n-----------------------");  
+                printf("\nESCOLHA UMA OPCAO:");
+                printf("\n1-Entrar com novos dados :");
+                printf("\n2-Listar Dados:");
+                printf("\n3-Sair:\n"); scanf("%d",&Opcao);
+                system("cls");
+                switch (Opcao)
+                {
+                case 1:
+                    Cadastro();
+                    break;
+                case 2:
+                    Listar();
+                    break;
+                case 3:
+                    printf("Saindo.......");
+                    printf("\n.....");
+                    break;
+                default:
+                    printf("Opcao invalida! Tente novamente.....\n");
+                    Menu();
+                    break;
+                }
 }
 void Cadastro()
 {
-    printf("Quantindade:"); scanf("%d",&total);
-     system("cls");
+    AlocarMemoria();
+          for(int i=0;i<total;i++)
+          {
+                printf("--------Cadastro Aluno %d--------\n",i+1);
+                printf("Aluno: "); scanf("%s",&aluno[i].nome);
+                printf("Matricula: "); scanf("%s",&aluno[i].cod);
+                printf("Media: "); scanf("%f",&aluno[i].media);
+          }
+                system("cls");
+                Arquivo();
+                printf("\n---------------");
+                printf("\nDados gravados!");
+                printf("\n---------------");
+                printf("\nVoltar ao menu?|1-Sim|ou|2-Sair|\n");scanf("%d",&Opcao);
+                system("cls");
+                Opcao==1?Menu():exit(1);
+    
+}
+void Arquivo()
+{
+     FILE *arqaluno;
+     arqaluno=fopen("DadosAluno.txt","a");
+               if(arqaluno==NULL)
+                 {
+                 printf("Erro na abertura do arquivo!");
+                 exit(1);
+                 }
+                //Gravando dados dos alunos em um arquivo
+                for(int i=0;i<total;i++)
+                 {
+                  fprintf(arqaluno,"--------------------------------------------- \n");
+                  fprintf(arqaluno,"Nome: %s| Matricula: %s | Media: %.2f \n",aluno[i].nome,aluno[i].cod,aluno[i].media);
+                 }
+                  fprintf(arqaluno,"--------------------------------------------- \n");
+    fclose(arqaluno);    
+                
+}
+void AlocarMemoria()
+{
+                printf("Quantindade de alunos:"); scanf("%d",&total);
+                system("cls");
+                //alocando memoria com malloc para minha estrura (vetor de registro) com a quantidade que usuario solicitar. 
+                aluno= ( DadosAluno *) malloc (total * sizeof(DadosAluno));
+                 if(!aluno)
+                {
+                    printf("Erro na alocação");
+                    exit(1);
+                }
+}
+void Listar()
+{
+                for(int i=0;i<total; i++)
+                {
+                    printf("\n---------------------------");
+                    printf("\nNome: %s",aluno[i].nome);
+                    printf("\nMatricula: %s",aluno[i].cod);
+                    printf("\nMedia: %.2f",aluno[i].media);
+                }
+                    printf("\n---------------------------");
 }
