0711a3164b2bbd8833b8b2063c125f7e4e9bf4e1
mateus varelo <mateusferreira703@gmail.com>
2020-11-09 09:48:41 -0300

    Create AV2-03.c

---
+++ CODIGO C PRO_ESTRU/AV2-03.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 2
+
+main(){
+
+
+  int i,j,x,Mat[TAM][TAM];
+  char booleano=0;
+
+  for (i=0;i<TAM;i++)
+  {
+     for(j=0;j<TAM;j++)
+     {
+       printf("\nValor p/ Linha[%d] Coluna[%d]: ",i,j);
+       scanf("%d",&Mat[i][j]);
+     }
+  }
+    printf("Digite valor para buscar:\n");scanf("%d",&x);
+   for(i=0;i<TAM;i++)
+   {
+      for(j=0;j<TAM;j++)
+      {
+        if(x==Mat[i][j])
+        {
+          booleano=1;
+          printf("\nValor encontrado!");
+          printf("Linha[%d] Coluna[%d]\n",i,j);
+        }
+      }
+   }
+     if(booleano==0)
+     {
+       printf("Nao encontrado!");
+     }
+  }
