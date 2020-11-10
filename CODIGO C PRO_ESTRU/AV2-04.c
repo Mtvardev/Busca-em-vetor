98f2d688c2538130e28597ecf4b3fbc17c5339fd
mateus varelo <mateusferreira703@gmail.com>
2020-11-09 23:01:49 -0300

    Create AV2-04.c

---
+++ CODIGO C PRO_ESTRU/AV2-04.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<locale.h>
+#define TAM 100
+void Troca(char *TrocaVogais);
+void Exibe(char *MostraFrase);
+main()
+{
+  setlocale(LC_ALL,"Portuguese");
+  char frase[TAM];
+       printf("\nDigite uma frase(Max:100 car.):");
+       fgets(frase,TAM,stdin);
+       Troca(&frase);
+       Exibe(&frase);    
+}
+void Troca(char *TrocaVogais)
+{
+     for(int i=0;i<strlen(TrocaVogais);i++)
+    {
+          switch(TrocaVogais[i])
+          {
+            case 'a':
+               TrocaVogais[i]='u';
+               break;
+            case 'e':
+                TrocaVogais[i]='o';
+                break;
+            case 'i':
+                TrocaVogais[i]='u';
+                break;
+            case 'o':
+                TrocaVogais[i]='a';
+                break;
+            case 'u':
+                TrocaVogais[i]='e';
+                break;
+          }
+    } 
+}
+void Exibe(char *MostraFrase)
+{
+    printf("\nFrase digitada: %s",MostraFrase);
+}
